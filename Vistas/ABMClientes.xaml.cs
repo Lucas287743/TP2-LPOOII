537d79716be9977e76416701fad3789182423607
Lucas Mesconi <mesconilucas2877@gmail.com>
2022-09-08 20:40:37 -0300

    Subida de Proyecto

---
+++ Vistas/ABMClientes.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClasesBase;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for ABMClientes.xaml
+    /// </summary>
+    public partial class ABMClientes : Window
+    {
+        public ABMClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            txtDni.Text = "";
+            txtApellido.Text = "";
+            txtNombre.Text = "";
+            txtDireccion.Text = "";
+
+            txtDni.IsEnabled = true;
+            txtApellido.IsEnabled = true;
+            txtNombre.IsEnabled = true;
+            txtDireccion.IsEnabled = true;
+
+            btnGuardar.IsEnabled = true;
+            btnCancelar.IsEnabled = true;
+
+            btnNuevo.IsEnabled = false;
+            btnModificar.IsEnabled = false;
+            btnEliminar.IsEnabled = false;
+            btnPrimero.IsEnabled = false;
+            btnSiguiente.IsEnabled = false;
+            btnAnterior.IsEnabled = false;
+            btnUltimo.IsEnabled = false;
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Cliente oCliente = new Cliente();
+            oCliente.Dni = txtDni.Text;
+            oCliente.Apellido = txtApellido.Text;
+            oCliente.Nombre = txtNombre.Text;
+            oCliente.Direccion = txtDireccion.Text;
+
+            MessageBoxResult msg = MessageBox.Show(oCliente.ToString(), "Confirmacion", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+            if (msg == MessageBoxResult.OK)
+            {
+                txtDni.IsEnabled = false;
+                txtApellido.IsEnabled = false;
+                txtNombre.IsEnabled = false;
+                txtDireccion.IsEnabled = false;
+
+                btnGuardar.IsEnabled = false;
+                btnCancelar.IsEnabled = false;
+
+                btnNuevo.IsEnabled = true;
+                btnModificar.IsEnabled = true;
+                btnEliminar.IsEnabled = true;
+                btnPrimero.IsEnabled = true;
+                btnSiguiente.IsEnabled = true;
+                btnAnterior.IsEnabled = true;
+                btnUltimo.IsEnabled = true;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            txtDni.Text = "";
+            txtApellido.Text = "";
+            txtNombre.Text = "";
+            txtDireccion.Text = "";
+
+            txtDni.IsEnabled = false;
+            txtApellido.IsEnabled = false;
+            txtNombre.IsEnabled = false;
+            txtDireccion.IsEnabled = false;
+
+            btnGuardar.IsEnabled = false;
+            btnCancelar.IsEnabled = false;
+
+            btnNuevo.IsEnabled = true;
+            btnModificar.IsEnabled = true;
+            btnEliminar.IsEnabled = true;
+            btnPrimero.IsEnabled = true;
+            btnSiguiente.IsEnabled = true;
+            btnAnterior.IsEnabled = true;
+            btnUltimo.IsEnabled = true;
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Vistas/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string rolUsuario = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string password = txtPassword.Password;
+
+            if (usuario == "Marcos" && password == "1234")
+            {
+                //Admin
+                rolUsuario = "Admin";
+                if (MessageBoxResult.OK == MessageBox.Show("Acceso correcto con el rol de Administador", "Login Exitoso", MessageBoxButton.OK, MessageBoxImage.Information))
+                {
+                    Menu oMenu = new Menu();
+                    oMenu.Show();
+                    this.Close();
+                }
+            }
+            else if (usuario == "Lucas" && password == "1234")
+            {
+                //Vendedor
+                rolUsuario = "Vendedor";
+                if (MessageBoxResult.OK == MessageBox.Show("Acceso correcto con el rol de Vendedor", "Login Exitoso", MessageBoxButton.OK, MessageBoxImage.Information))
+                {
+                    Menu oMenu = new Menu();
+                    oMenu.Show();
+                    this.Close();
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Usuario y/o contraseña son incorrectas","Alerta",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+    }
+}
